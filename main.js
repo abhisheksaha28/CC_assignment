ef16c4fcf1e0864b5e96e058537ab44a6800bcd0
Abhishek Saha <110456088+abhisheksaha28@users.noreply.github.com>
2024-11-03 23:09:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//navbar menu//
+const ham = document.getElementById('mobile')
+const header = document.getElementById('header')
+ham.addEventListener('click', () => header.classList.toggle('show'));
